cb9269466fbba3171e9c5c980aa53ec746f871d0
Pali Frotan <pali.frotan@icloud.com>
2023-06-05 20:53:55 +0100

    Feat:  API used, returns url of a kitten pic

--- exercise002.js
+++ exercise002.js
@@ -1,13 +1,13 @@
 import fetch from "node-fetch";
 
-const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+const jsonTypicode = "https://placekitten.com/g/200/300";
 
 const fetchData = async (apiEndPoint) => {
   try 
     {
       const response = await fetch(apiEndPoint);
-      const jsonResponse = await response.json();
-      console.log(jsonResponse);
+      const urlResponse = response.url;
+      console.log(urlResponse);
     } 
   catch(error) 
     {console.log(error);}
